95569c6ad6955f41f9ee572fcddeeabcb85a7f33
Pedro Carrasco <pedrodonte@gmail.com>
2020-07-30 20:12:57 -0400

    fix1

--- code/create.py
+++ code/create.py
@@ -19,7 +19,7 @@ def create(event, context):
         return {'statusCode': 422,
                 'body': json.dumps({'error_message': 'Couldn\'t create the todo item. As text was empty.'})}
 
-    agenda_id = data['tipo_evento']+"_"+data['paciente_id']
+    agenda_id = data['fecha_evento']+"_"+data['paciente_id']
     a_todo = AgendaModel(agenda_id=agenda_id,
                        paciente_id=data['paciente_id'],
                        tipo_evento=data['tipo_evento'],
